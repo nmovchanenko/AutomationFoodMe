258836a3cb4c3acf4517c392f25133da18dc0e0d
Nikolay Movchanenko <movchanenko.nn@gmail.com>
2019-11-28 15:46:15 +0300

    set default customer

--- app/js/services/customer.js
+++ app/js/services/customer.js
@@ -5,10 +5,7 @@ foodMeApp.factory('customer', function($rootScope, localStorage) {
   var LOCAL_STORAGE_ID = 'fmCustomer',
       customerString = localStorage[LOCAL_STORAGE_ID];
 
-  var customer = customerString ? JSON.parse(customerString) : {
-    name: undefined,
-    address: undefined
-  };
+  var customer = customerString ? JSON.parse(customerString) : {name: "Joe Black", address: "432 Wiggly Rd, Mountain View, 94043"};
 
   $rootScope.$watch(function() { return customer; }, function() {
     localStorage[LOCAL_STORAGE_ID] = JSON.stringify(customer);
