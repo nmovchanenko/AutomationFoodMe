c9abefab34bde3760d1e0beff0709225c9356b05
Nikolay Movchanenko <movchanenko.nn@gmail.com>
2019-11-28 15:42:03 +0300

    Revert "set hard-coded customer"
    This reverts commit f865b44b

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 foodMeApp.controller('RestaurantsController',
-    function RestaurantsController($scope, customer = {name: "Joe Black", address: "432 Wiggly Rd, Mountain View, 94043"}, $location, Restaurant) {
+    function RestaurantsController($scope, customer, $location, Restaurant) {
 
   if (!customer.address) {
     $location.url('/customer');
